791dcfe8079bf041e4de9c9cad2ed48ce69f3736
DSchreyer <daniel-schreyer@web.de>
2022-02-13 17:08:06 +0000

    Small fixes

--- bin/get_region_coverage.py
+++ bin/get_region_coverage.py
@@ -9,22 +9,20 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', metavar='',
-                                  help="Input bam file")
+                    help="Input bam file")
 # parser.add_argument('-o', '--output', metavar='',
 #                                   help="Output bam file")
 parser.add_argument('-b', '--bed', metavar='',
-                                  help="CIRCexplorer parse output in bed format")
-parser.add_argument('-d', '--directory', metavar='',
-                                  help="Directory in which output is saved", 
-                                  default = "./")
-parser.add_argument('-o', '--output', metavar='',
-                                  help="Output bed file with coverage information")
-parser.add_argument('-o', '--output', metavar='',
-                                  help="Output bed file with coverage information")
-parser.add_argument('-o', '--output', metavar='',
-                                  help="Output bed file with coverage information")
-parser.add_argument('-o', '--output', metavar='',
-                                  help="Output bed file with coverage information")
+                    help="CIRCexplorer parse output in bed format")
+parser.add_argument('-q', '--mapq', metavar='',
+                    help="Mapping quality threshold for split reads",
+                    default = 20)
+parser.add_argument('--bases', metavar='',
+                    help = "Number of bases for computing the coverage at junction",
+                    default = 200)
+parser.add_argument('-e', '--extension', metavar='',
+                    help="Number of bases inside the circDNA breakpoint to comput the coverage ration",
+                    default = 100)
 
 
 parser.parse_args()
@@ -33,6 +31,10 @@ sorted_bam = args.input
 directory = os.path.dirname(args.input)
 bam_file = os.path.basename(args.input)
 
-coverage_object = coverage(bam_file, bt.BedTool(args.bed),300,20,150, directory)
+input_file, input_file_extension = os.path.splitext(os.path.basename(args.input))
+output = directory + "/" + input_file + "_coverage.bed"
+
+coverage_object = coverage(bam_file, bt.BedTool(args.bed), args.bases, args.mapq,
+                        args.extension, directory)
 bed_coverage = coverage_object.compute_coverage(coverage_object.get_wg_coverage())
-bt.BedTool(bed_coverage).saveas( args.output )
+bt.BedTool(bed_coverage).saveas( output )
