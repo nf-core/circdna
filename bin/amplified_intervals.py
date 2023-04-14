5782325b1178be753b815a6797d0bfa7a9309112
DSchreyer <daniel-schreyer@web.de>
2023-04-14 16:22:17 +0100

    Added conda for prepareaa

--- bin/amplified_intervals.py
+++ bin/amplified_intervals.py
@@ -35,90 +35,52 @@ import global_names
 GAIN = 4.5
 CNSIZE_MIN = 50000
 
-parser = argparse.ArgumentParser(description="Filter and merge amplified intervals")
-parser.add_argument(
-    "--bed",
-    dest="bed",
-    help="Input bed file with list of amplified intervals",
-    metavar="FILE",
-    action="store",
-    type=str,
-    required=True,
-)
-parser.add_argument(
-    "--out",
-    dest="out",
-    help="OPTIONAL: Prefix filename for output bed file. Default: <INPUT_BED_BASENAME>_amplified.bed",
-    metavar="FILE",
-    action="store",
-    type=str,
-    default="",
-)
-parser.add_argument(
-    "--bam",
-    dest="bam",
-    help="OPTIONAL: Bamfile, used to avoid large aneuploidies",
-    metavar="FILE",
-    action="store",
-    type=str,
-    default="",
-)
-parser.add_argument(
-    "--gain",
-    dest="gain",
-    help="OPTIONAL: CN gain threshold for interval to be considered as a seed. Default: 5",
-    action="store",
-    type=float,
-    default=GAIN,
-)
-parser.add_argument(
-    "--cnsize_min",
-    dest="cnsize_min",
-    help="OPTIONAL: Minimum size (in bp) for interval to be considered as a seed. Default: 100000",
-    action="store",
-    type=int,
-    default=CNSIZE_MIN,
-)
-parser.add_argument(
-    "--ref",
-    dest="ref",
-    help='Values: [hg19, GRCh37, GRCh38, None]. "hg19"(default) & "GRCh38" : chr1, .. chrM etc / "GRCh37" : \'1\', \'2\', .. \'MT\' etc/ "None" : Do not use any annotations. AA can tolerate additional chromosomes not stated but accuracy and annotations may be affected. Default: hg19',
-    metavar="STR",
-    action="store",
-    type=str,
-    default="hg19",
-)
-parser.add_argument(
-    "--no_cstats",
-    dest="no_cstats",
-    help="Do not re-use coverage statistics from coverage.stats.",
-    action="store_true",
-    default=False,
-)
 
+parser = argparse. \
+    ArgumentParser(description="Filter and merge amplified intervals")
+parser.add_argument('--bed', dest='bed',
+                    help="Input bed file with list of amplified intervals", metavar='FILE',
+                    action='store', type=str, required=True)
+parser.add_argument('--out', dest='out',
+                    help="OPTIONAL: Prefix filename for output bed file. Default: <INPUT_BED_BASENAME>_amplified.bed",
+                    metavar='FILE',
+                    action='store', type=str, default='')
+parser.add_argument('--bam', dest='bam',
+                    help="OPTIONAL: Bamfile, used to avoid large aneuploidies", metavar='FILE',
+                    action='store', type=str, default='')
+parser.add_argument('--gain', dest='gain',
+                    help="OPTIONAL: CN gain threshold for interval to be considered as a seed. Default: 5",
+                    action='store', type=float, default=GAIN)
+parser.add_argument('--cnsize_min', dest='cnsize_min',
+                    help="OPTIONAL: Minimum size (in bp) for interval to be considered as a seed. Default: 100000",
+                    action='store', type=int, default=CNSIZE_MIN)
+parser.add_argument('--ref', dest='ref',
+                    help="Values: [hg19, GRCh37, GRCh38, GRCh38_viral, mm10, GRCm38]. \"hg19\", \"GRCh38\", \"mm10\" : chr1, .. chrM etc / \"GRCh37\", \"GRCm38\" : '1', '2', .. 'MT' etc/ \"None\" : Do not use any annotations. AA can tolerate additional chromosomes not stated but accuracy and annotations may be affected.", metavar='STR',
+                    action='store', type=str, choices=["hg19", "GRCh37", "GRCh38", "GRCh38_viral", "mm10", "GRCm38"], required=True)
+parser.add_argument('--no_cstats', dest='no_cstats', help="Do not re-use coverage statistics from coverage.stats.",
+                    action='store_true', default=False)
 
 args = parser.parse_args()
 
 global_names.REF = args.ref
 import ref_util as hg
 
-if args.bed != "":
+if args.bed != '':
     rdAlts = args.bed
 
-if args.out != "":
+if args.out != '':
     outname = args.out + ".bed"
 else:
     outname = os.path.splitext(rdAlts)[0] + "_amplified.bed"
 
 GAIN, CNSIZE_MIN = args.gain, args.cnsize_min
 
-rdList0 = hg.interval_list(rdAlts, "bed")
+rdList0 = hg.interval_list(rdAlts, 'bed')
 if rdList0:
     try:
         if len(rdList0[0].info) == 0:
-            sys.stderr.write(
-                "ERROR: CNV estimate bed file had too few columns.\n" "Must contain: chr  pos1  pos2  cnv_estimate\n"
-            )
+            sys.stderr.write("ERROR: CNV estimate bed file had too few columns.\n"
+                             "Must contain: chr  pos1  pos2  cnv_estimate\n")
             sys.exit(1)
         _ = float(rdList0[0].info[-1])
 
@@ -126,15 +88,24 @@ if rdList0:
         sys.stderr.write("ERROR: CNV estimates must be in last column of bed file.\n")
         sys.exit(1)
 
-rdList = hg.interval_list([r for r in rdList0 if float(r.info[-1]) > GAIN])
+tempL = []
+for r in rdList0:
+    if args.ref == "GRCh38_viral" and not r.chrom.endswith("chr"):
+        tempL.append(r)
+
+    elif float(r.info[-1]) > GAIN:
+        tempL.append(r)
+
+rdList = hg.interval_list(tempL)
+
+# rdList = hg.interval_list([r for r in rdList0 if float(r.info[-1]) > GAIN or (args.ref == "GRCh38_viral" and not r.chrom.endswith("chr"))])
 
 if args.bam != "":
     import bam_to_breakpoint as b2b
-
-    if os.path.splitext(args.bam)[-1] == ".cram":
-        bamFile = pysam.Samfile(args.bam, "rc")
+    if os.path.splitext(args.bam)[-1] == '.cram':
+        bamFile = pysam.Samfile(args.bam, 'rc')
     else:
-        bamFile = pysam.Samfile(args.bam, "rb")
+        bamFile = pysam.Samfile(args.bam, 'rb')
 
     cstats = None
     cb = bamFile
@@ -146,10 +117,11 @@ if args.bam != "":
             bamfile_filesize = os.path.getsize(bamfile_pathname)
             if ll[0] == os.path.abspath(bamfile_pathname):
                 cstats = tuple(map(float, ll[1:]))
-                if len(cstats) < 15 or cstats[13] != 3 or bamfile_filesize != int(cstats[14]):
+                if len(cstats) < 15 or cstats[13] != 3 or bamfile_filesize != int(cstats[14]) or any(np.isnan(cstats)):
                     cstats = None
 
         coverage_stats_file.close()
+
 
     bamFileb2b = b2b.bam_to_breakpoint(bamFile, coverage_stats=cstats)
     pre_int_list = []
@@ -157,40 +129,32 @@ if args.bam != "":
         try:
             chrom_cov_ratio = bamFileb2b.median_coverage(refi=r)[0] / bamFileb2b.median_coverage()[0]
             # print("chrom ratio " + r.chrom + " " + str(chrom_cov_ratio))
-            if (
-                float(r.info[-1])
-                > GAIN
-                + 2
-                * max(
-                    1.0,
-                    bamFileb2b.median_coverage(refi=r)[0] / bamFileb2b.median_coverage()[0],
-                )
-                - 2
-                and bamFileb2b.median_coverage(refi=r)[0] / bamFileb2b.median_coverage()[0] > 0
-            ):
-                if r.size() < 10000000 or float(r.info[-1]) > 1.5 * GAIN:
+            if float(r.info[-1]) > GAIN + 2 * max(1.0, bamFileb2b.median_coverage(refi=r)[0] / bamFileb2b.median_coverage()[0]) - 2 and \
+                    bamFileb2b.median_coverage(refi=r)[0] / bamFileb2b.median_coverage()[0] > 0:
+                if r.size() < 10000000 or float(r.info[-1]) > 1.5*GAIN:
                     pre_int_list.append(r)
 
+            elif float(r.info[-1]) > 1 and args.ref == "GRCh38_viral" and not r.chrom.startswith("chr"):
+                pre_int_list.append(r)
+
         except ZeroDivisionError:
+            print(r.chrom, args.ref, float(r.info[-1]))
+            # if float(r.info[-1]) > 1 and args.ref == "GRCh38_viral" and not r.chrom.startswith("chr"):
+            #     pre_int_list.append(r)
+            #
             continue
 
     rdList = hg.interval_list(pre_int_list)
 
-genome_features = hg.oncogene_list
 amplicon_listl = rdList
 
 cr = hg.conserved_regions
 uc_list = hg.interval_list([])
 for a in amplicon_listl:
-    if (
-        len(hg.interval_list([a]).intersection(cr)) == 0
-        or a.size()
-        > max(
-            1000000,
-            10 * sum([a.intersection(ci[1]).size() for ci in hg.interval_list([a]).intersection(cr)]),
-        )
-        or a.size() - sum([a.intersection(ci[1]).size() for ci in hg.interval_list([a]).intersection(cr)]) > 2000000
-    ):
+    if (len(hg.interval_list([a]).intersection(cr)) == 0 or
+            a.size() > max(1000000,
+                           10 * sum([a.intersection(ci[1]).size() for ci in hg.interval_list([a]).intersection(cr)])) or
+            a.size() - sum([a.intersection(ci[1]).size() for ci in hg.interval_list([a]).intersection(cr)]) > 2000000):
         if (len(hg.interval_list([a]).intersection(cr))) == 0:
             uc_list.append(a)
         else:
@@ -203,21 +167,24 @@ for a in amplicon_listl:
             if a.end > cpos:
                 uc_list.append(hg.interval(a.chrom, cpos, a.end, info=a.info))
 
-uc_list = hg.interval_list(
-    [a for a in uc_list if float(a.info[-1]) * a.segdup_uniqueness() > GAIN and a.rep_content() < 2.5]
-)
-uc_merge = uc_list.merge_clusters(extend=300000)
+new_uc_list = []
+for a in uc_list:
+    if args.ref == "GRCh38_viral" and not a.chrom.startswith("chr"):
+        if a.rep_content() < 2.5:
+            new_uc_list.append(a)
+    else:
+        if float(a.info[-1]) * a.segdup_uniqueness() > GAIN and a.rep_content() < 2.5:
+            new_uc_list.append(a)
+
+uc_merge = hg.interval_list(new_uc_list).merge_clusters(extend=300000)
 
 with open(outname, "w") as outfile:
     for a in uc_merge:
-        if sum([ai.size() for ai in a[1]]) > CNSIZE_MIN:
-            outfile.write(
-                "\t".join(
-                    [
-                        str(a[0]),
-                        str(sum([ai.size() * float(ai.info[-1]) for ai in a[1]]) / sum([ai.size() for ai in a[1]])),
-                        rdAlts,
-                    ]
-                )
-                + "\n"
-            )
+        is_viral = False
+        if args.ref == "GRCh38_viral" and not a[0].chrom.startswith("chr"):
+            is_viral = True
+
+        if sum([ai.size() for ai in a[1]]) > CNSIZE_MIN or is_viral:
+            outfile.write('\t'.join(
+                [str(a[0]), str(sum([ai.size() * float(ai.info[-1]) for ai in a[1]]) / sum([ai.size() for ai in a[1]])),
+                 rdAlts]) + '\n')
--- modules/local/prepareaa.nf
+++ modules/local/prepareaa.nf
@@ -2,10 +2,15 @@ process PREPAREAA {
     tag "$meta.id"
     label 'process_low'
 
-    conda "conda-forge::python=2.7 bioconda::pysam=0.15.2 anaconda::flask=1.1.2 anaconda::cython=0.29.14 anaconda::numpy=1.16.6 anaconda::scipy=1.2.1 conda-forge::matplotlib=2.2.5 mosek::mosek=8.0.60 anaconda::future=0.18.2 anaconda::intervaltree=3.0.2"
+    //conda "conda-forge::python=3.6.15 anaconda::intervaltree=3.0.2 conda-forge::numpy=1.19.5 bioconda::pysam=0.15.3"
+    //conda "conda-forge::python=2.7 bioconda::pysam=0.15.2 anaconda::flask=1.1.2 anaconda::cython=0.29.14 anaconda::numpy=1.16.6 anaconda::scipy=1.2.1 conda-forge::matplotlib=2.2.5 mosek::mosek=8.0.60 anaconda::future=0.18.2 anaconda::intervaltree=3.0.2"
+    //conda "conda-forge::python=2.7 bioconda::pysam=0.15.2 anaconda::numpy=1.16.6 conda-forge::matplotlib=2.2.5 mosek::mosek=8.0.60 anaconda::intervaltree=3.0.2 anaconda::scipy=1.2.1"
+
+    conda "conda-forge::python=2.7 bioconda::pysam=0.20.0 anaconda::flask=1.1.2 conda-forge::numpy=1.16.5 conda-forge::matplotlib=2.2.4 anaconda::scipy=1.2.0 mosek::mosek=8.0.60 anaconda::intervaltree=3.0.2 anaconda::future=0.18.2"
     container "${ workflow.containerEngine == 'singularity' && !task.ext.singularity_pull_docker_container ?
         'https://depot.galaxyproject.org/singularity/mulled-v2-6eefa51f13933d65b4f8155ca2f8cd81dea474ba:baa777f7c4e89a2ec4d1eab7d424a1f46503bac7-0':
         'quay.io/biocontainers/mulled-v2-6eefa51f13933d65b4f8155ca2f8cd81dea474ba:baa777f7c4e89a2ec4d1eab7d424a1f46503bac7-0' }"
+
 
     input:
     tuple val(meta), path(bam), path(cns)
@@ -29,6 +34,7 @@ process PREPAREAA {
     """
     export AA_DATA_REPO=${params.aa_data_repo}
     export MOSEKLM_LICENSE_FILE=${params.mosek_license_dir}
+    export AA_SRC=${projectDir}/bin
     REF=${params.reference_build}
 
     PrepareAA.py \\
